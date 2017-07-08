be7486ad3d8961e23fdef06f61d35848a18f2659
Rafael Espinoza <nerddiffer@gmail.com>
2017-07-08 12:00:03 -0700

    Update shell aliases

--- settings/aliases.zsh
+++ settings/aliases.zsh
@@ -1,5 +1,32 @@
-ALIASES=$DOT_FILES_DIR/bash/bash_aliases
+# Overwrite, mix & match your own aliases here...
 
-if [ -f $ALIASES ]; then
-  source $ALIASES
-fi
+# git aliases
+alias gbr='git branch'
+alias gci='git commit'
+alias gco='git checkout'
+alias gst='git status'
+alias ga='git add'
+
+# other commands
+alias ..='cd ..'
+alias ...='cd ../../'
+alias ....='cd ../../../'
+alias c='clear'
+alias diff='colordiff'
+
+# aliases provided with a fresh install of Manjaro
+alias l='ls -CF'
+alias ls='ls --group-directories-first --time-style=+"%m.%d.%Y %H:%M" --color=auto -F'
+alias ll='ls --group-directories-first --time-style=+"%m.%d.%Y %H:%M" --color=auto -Fl'
+alias la='ls --group-directories-first --time-style=+"%m.%d.%Y %H:%M" --color=auto -Fla'
+alias grep='grep --color=tty -d skip'
+alias df='df -h'                      # human-readable sizes
+alias free='free -m'                  # show sizes in MB
+alias np='nano PKGBUILD'
+alias fixit='sudo rm -f /var/lib/pacman/db.lck && sudo pacman-mirrors -g && sudo pacman -Syyuu  && sudo pacman -Suu'
+
+# some safety nets
+alias rm='rm -I'
+alias mv='mv -i'
+alias cp='cp -i'
+alias ln='ln -i'
